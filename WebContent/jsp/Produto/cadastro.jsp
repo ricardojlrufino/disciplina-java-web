164466a53dcc74bd883628a09bb9f4dfbf2453f4
ricardojlrufino <ricardo.jl.rufino@gmail.com>
2013-09-04 23:37:05 -0300

    aula05 - introducao ao JSP

---
+++ WebContent/jsp/Produto/cadastro.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Cadastro de Produto</title>
+</head>
+<body>
+
+<form action="produto" method="post">
+<fieldset>
+	<legend>Cadastro de Produto</legend>
+	Descrição : <input name="descricao"> <br/>
+	Preço : <input name="preco"> <br/>
+	<button type="submit">Salvar</button>
+</fieldset>	
+</form>
+
+</body>
+</html>
--- src/br/com/cet/exercicio/ProdutoServlet.java
+++ src/br/com/cet/exercicio/ProdutoServlet.java
@@ -2,6 +2,7 @@ package br.com.cet.exercicio;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +16,12 @@ public class ProdutoServlet extends CrudBaseServlet {
        
     public ProdutoServlet() {
         super();
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("jsp/Produto/cadastro.jsp");
+    	dispatcher.forward(request, response);
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
